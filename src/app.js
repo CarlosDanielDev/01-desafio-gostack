66147e92493498cb570a23ca48d7ed0937447dd1
DanPHP7 <killer.dk20@gmail.com>
2020-04-10 12:37:07 -0300

    feat: create repository && list repository

--- src/app.js
+++ src/app.js
@@ -1,21 +1,35 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+function logRequests(req, res, next) {
+  const { method, url } = req;
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+  console.time(logLabel);
+  next();
+  console.timeEnd(logLabel);
+}
 const repositories = [];
 
+// aplicando middlewares de logs nas rotas
+app.use(logRequests);
+
+// listagem de repositórios
 app.get("/repositories", (request, response) => {
   // TODO
+  return res.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repository);
+  return response.status(200).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
